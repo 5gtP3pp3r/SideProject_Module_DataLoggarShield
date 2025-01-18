79924c188058c7749455b3f02d652a8609eccebb
William <william.crepault@gmail.com>
2025-01-18 15:44:13 -0500

    Ajout classes Program, non complet

---
+++ include/Program.h
@@ -0,0 +1,13 @@
+#include "CSV_TelemetryLogger.h"
+#include "Modules/SD_Shield.h"
+#include "Modules/DS1307Clock.h"
+
+class Program {
+private:
+public:
+    Program();
+};
+
+
+
+// Rien de spécifié pour l'instant, Je veux assembler les modules ADXL345 et mon SD_Shield. Les borniers ne sont pas soudés ;)
