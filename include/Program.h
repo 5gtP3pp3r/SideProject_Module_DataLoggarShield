5c1d0aeec869e3d085afcd06e258418848515a23
William <william.crepault@gmail.com>
2025-01-18 16:01:13 -0500

    Ajout comms Program

--- include/Program.h
+++ include/Program.h
@@ -11,3 +11,4 @@ public:
 
 
 // Rien de spécifié pour l'instant, Je veux assembler les modules ADXL345 et mon SD_Shield. Les borniers ne sont pas soudés ;)
+// À ajouter à ce Program de test: l'objet BMP280Sensor pour tester l'éciture de données dans un .csv d'une carte SD
